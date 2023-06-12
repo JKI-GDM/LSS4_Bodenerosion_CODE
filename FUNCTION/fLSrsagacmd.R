5080fb236265329ebee5334eaffc985597825a43
JKI's Geodata Management <127733040+JKI-GDM@users.noreply.github.com>
2023-06-12 07:02:32 +0200

    Update fLSrsagacmd.R

--- FUNCTION/fLSrsagacmd.R
+++ FUNCTION/fLSrsagacmd.R
@@ -1,18 +1,7 @@
 print("fLSrsagacmd: Calculation of LS factor using SAGA GIS and Rsagacmd")
 #########################################################################################################
-##Libraries and functions
+##Initiating saga object
 #########################################################################################################
-loadandinstall <- function(mypkg) {
-  for(i in seq(along=mypkg)){
-    if (!is.element(mypkg[i],installed.packages()[,1])){install.packages(mypkg[i])}
-    library(mypkg[i], character.only=TRUE)
-  }
-}
-packages <- sort(c("Rsagacmd",
-                   "gtools",
-                   "terra",
-                   "raster"))
-loadandinstall(packages)
 
 print("[1] Initiating saga object")
 saga <- saga_gis(raster_backend = "terra",
@@ -26,6 +15,7 @@ fLSrsagacmd <- function(DATA.DIR,
                 DEM.FRM,
                 EPSG,
                 PARCEL,
+                ASC.DIR,
                 ASC.EXPORT=TRUE){
 
 #------------------------------------------------------------------------------- 
@@ -40,12 +30,11 @@ print("[3] Filling sinks")
 saga$ta_preprocessor$fill_sinks_planchon_darboux_2001(dem=paste(DATA.DIR,DEM.FILE,".sgrd",sep=""),
                                                       result=paste(DATA.DIR,DEM.FILE,"_FILL.sgrd",sep=""))
   
-
 #------------------------------------------------------------------------------- 
 print("[4] LS factor without boundaries")
 #-------------------------------------------------------------------------------
 saga$ta_hydrology$ls_factor_field_based(dem=paste(DATA.DIR,DEM.FILE,"_FILL.sgrd",sep=""),
-                                          ls_factor=paste(DATA.DIR,DEM.FILE,"_nB-LS.sgrd",sep=""),
+                                          ls_factor=paste(ASC.DIR,DEM.FILE,"_LSnB.sgrd",sep=""),
                                           method=0)
   
   
@@ -54,20 +43,20 @@ print("[5] Field-based LS factor")
 #-------------------------------------------------------------------------------
 saga$ta_hydrology$ls_factor_field_based(dem=paste(DATA.DIR,DEM.FILE,"_FILL.sgrd",sep=""),
                                           fields = paste(DATA.DIR,PARCEL,sep=""),
-                                          ls_factor=paste(DATA.DIR,DEM.FILE,"_wB-LS.sgrd",sep=""),
+                                          ls_factor=paste(ASC.DIR,DEM.FILE,"_LSwB.sgrd",sep=""),
                                           method=0)
 
 if(ASC.EXPORT==TRUE){
 #------------------------------------------------------------------------------- 
 print("[6] asc export")
 #-------------------------------------------------------------------------------
-setwd(file.path(DATA.DIR))
+setwd(file.path(ASC.DIR))
 l.g <- mixedsort(list.files(pattern=paste("^(",DEM.FILE,").*\\.sdat$",sep="")),decreasing=TRUE)
 print(l.g)
 pb <- txtProgressBar(min=1, max=length(l.g), style=3)
 for(i in 1:length(l.g)){
-  r <- raster(paste(DATA.DIR,l.g[i],sep=""))
-  writeRaster(r,paste(DATA.DIR,substr(l.g[i],1,nchar(l.g[i])-5),".asc",sep=""),overwrite=TRUE)
+  r <- raster(paste(ASC.DIR,l.g[i],sep=""))
+  writeRaster(r,paste(ASC.DIR,substr(l.g[i],1,nchar(l.g[i])-5),".asc",sep=""),overwrite=TRUE)
 setTxtProgressBar(pb, i)
 }
 }
